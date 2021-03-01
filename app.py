d5f52fc6660aa345b925b23c551bfe4880e0ef6e
George <itsalexandrov@gmail.com>
2021-03-01 15:05:39 +0300

    added auth managers

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Request
 from spotify_backend import generate_auth_url
 
 app = Flask(__name__)
 
 spotify_storage = {}
-
+auth_managers = {}
 
 @app.route('/')
 def index():
     return render_template('index.html', url=generate_auth_url()[1])
 
+@app.route('/oauth')
+def oauth():
+    # TODO: oauth
+    pass
 
 if __name__ == '__main__':
     app.run()
