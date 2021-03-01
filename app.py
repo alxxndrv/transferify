8618d38638ab1e8e64e506ea6f36fd556d1980b0
George <itsalexandrov@gmail.com>
2021-03-01 15:06:39 +0300

    code format

--- app.py
+++ app.py
@@ -6,14 +6,17 @@ app = Flask(__name__)
 spotify_storage = {}
 auth_managers = {}
 
+
 @app.route('/')
 def index():
     return render_template('index.html', url=generate_auth_url()[1])
+
 
 @app.route('/oauth')
 def oauth():
     # TODO: oauth
     pass
+
 
 if __name__ == '__main__':
     app.run()
