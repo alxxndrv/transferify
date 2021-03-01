84562ff50adaba3d99d12310fc86419de69ce480
George <itsalexandrov@gmail.com>
2021-03-01 15:23:55 +0300

    generate user id

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 from spotify_backend import generate_auth_url
+from uuid import uuid4
 
 app = Flask(__name__)
 
@@ -9,9 +10,25 @@ spotify_storage = {}
 auth_managers = {}
 
 
+def generate_session_id():
+    return str(uuid4())
+
+
 @app.route('/')
 def index():
-    return render_template('index.html', url=generate_auth_url()[1])
+    auth_manager, url = generate_auth_url()
+    session_id = generate_session_id()
+    auth_managers[session_id] = auth_manager
+    response = make_response(render_template('index.html', url=url))
+    user_id = request.cookies.get('userID')
+    if not user_id:  # Проверяем, есть ли в куках userID
+        user_id = generate_session_id()
+        response.set_cookie('userID', user_id)
+
+    if not auth_managers.get(user_id):
+        auth_managers[user_id] = auth_manager
+
+    return response
 
 
 @app.route('/oauth')
