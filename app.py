a9abd153da684647a3788c9a018c908068aaa456
George <itsalexandrov@gmail.com>
2021-03-01 16:09:38 +0300

    oauth

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, make_response
-from spotify_backend import generate_auth_url
+from flask import Flask, render_template, request, make_response, redirect
+from spotify_backend import generate_auth_url, create_spotify
 from uuid import uuid4
+import spotipy
 
 app = Flask(__name__)
 
@@ -31,10 +32,23 @@ def index():
     return response
 
 
+@app.route('/transfer')
+def transfer_page():
+    return 'transfer_page'
+
+
 @app.route('/oauth')
 def oauth():
-    # TODO: oauth
-    pass
+    # TODO: проверка, есть ли user_id в auth и в spotify
+    user_id = request.cookies.get('userID')
+    auth_manager = auth_managers.get(user_id)  # Достаем auth manager из словаря
+    if not auth_manager:
+        response = make_response(redirect('/'))
+        response.set_cookie('userID', '', expires=0)
+        return response
+    auth_manager.parse_auth_response_url(request.url)  # Парсим урл оауфа
+    spotify_storage[user_id] = create_spotify(auth_manager)
+    return redirect('transfer')
 
 
 if __name__ == '__main__':
--- spotify_backend.py
+++ spotify_backend.py
@@ -8,3 +8,8 @@ def generate_auth_url():
     auth_manager = SpotifyOAuth(scope=scope)
     url = auth_manager.get_authorize_url()
     return auth_manager, url
+
+
+def create_spotify(auth_manager):
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    return spotify
